9974751490f69405e67462b9388a86c74f16fdca
devGEP <ju0032564@gmail.com>
2023-07-05 01:50:19 +0900

    feat: 초기세팅 완료(tailwind 제외)

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  // eslint-disable-next-line quotes
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
